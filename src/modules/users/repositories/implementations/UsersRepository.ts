3804e8e2f7ad5285fdba3b917ab99f8413333a4b
rafelis1997 <rafaelheros80@gmail.com>
2022-09-30 16:37:47 -0300

    final delivery

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,24 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOne({
+      relations: ['games'],
+      where:{
+        id: user_id
+      }
+    }) as User;
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    const users = this.repository.query("SELECT * FROM users ORDER BY first_name;"); // Complete usando raw query 
+    return users;
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users WHERE first_name ILIKE '%${first_name}%' AND last_name ILIKE '%${last_name}%';`); // Complete usando raw query
   }
 }
